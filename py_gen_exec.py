a518d333a6646da7d5fcf3d852fd63208d38ff34
andreytata <andreytata@ex.ua>
2019-06-07 14:27:13 +0300

    scan dirs not finished!

--- py_gen_exec.py
+++ py_gen_exec.py
@@ -7,3 +7,32 @@ script find line with source glTF2.0 file. Folder can contain more then one file
 used ".gltf" extention, generator must use only one, defined in projects *.pri
 file."""
 
+import os, sys, re, json
+
+gx_gen_regexp = re.compile("^gx_gen_([_a-z][_a-z0-9]*)$")
+
+def generate_source_from_gltf(target_dir, match):
+    print('  -o-BEGIN target_dir "%s"' % target_dir)
+    print('   | match.groups()    %s' % repr(match.groups()))
+    config = open(os.path.join(target_dir, "py_gen_util.json"), "r")
+    config = json.loads(config.read())
+    print('   | config            %s' % config )
+    gltf_file_path = os.path.join(target_dir, config['gltf'])
+    print('   | gltf_file_path    "%s"' % gltf_file_path)
+    if not os.path.isfile(gltf_file_path):
+        raise IOError(gltf_file_path) 
+    print('  -o-END "%s"' % target_dir)
+
+if __name__=='__main__':
+    print('''Python %s''' % sys.version)
+    print('''os.getcwd() => "%s"''' % os.getcwd())
+    utils_path = os.path.normpath(os.path.split(os.path.abspath(sys.argv[0]))[0])
+    parsed_dir = os.path.split(utils_path)[0]
+    print('utils_path "%s"' % utils_path)
+    print('parsed_dir "%s"' % parsed_dir)
+    for name in os.listdir(parsed_dir):
+        print("  ?%s" % os.path.join(parsed_dir, name))
+        match = gx_gen_regexp.match(name)
+        if match:
+            target_dir = os.path.join(parsed_dir, name)
+            generate_source_from_gltf(target_dir, match)
