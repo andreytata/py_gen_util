f341eef0e3215ae1a955afeff97b61cdf3a581a4
andreytata <andreytata@ex.ua>
2019-06-25 17:01:56 +0300

    generation gap abstract interface added

---
+++ gx_cpp_face.py
@@ -0,0 +1,236 @@
+gx_hpp_face_template = """
+#ifndef GX_GENERATIONGAP_BASE_H
+#define GX_GENERATIONGAP_BASE_H
+
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+#include <QVector2D>
+#include <QVector3D>
+#include <QVector4D>
+#include <QObject>
+
+namespace geom
+{
+    class AbstractFactory: virtual public QObject
+    {
+        Q_OBJECT
+    public:
+        AbstractFactory(){}
+        virtual ~AbstractFactory(){}
+    };
+
+    class GENERATED_FACTORY;
+}
+
+struct MeshVertexData {  // P3N3T2
+    QVector3D position;
+    QVector3D normal;
+    QVector2D texCoord;
+};
+
+struct SkinVertexData {  // P3N3W4J4T2
+    QVector3D position;
+    QVector3D normal;
+    QVector4D weights_0;
+    QVector4D joints_0;
+    QVector2D texCoord;
+};
+
+class GeometryEngine : protected QOpenGLFunctions
+{
+public:
+    GeometryEngine();
+    virtual ~GeometryEngine();
+    virtual int get_indices_count();
+    virtual int get_vertices_count();
+    virtual int get_vertex_size();
+    virtual void drawGeometry(QOpenGLShaderProgram *program);
+    virtual void initGeometry();
+
+protected:
+    QOpenGLBuffer arrayBuf;
+    QOpenGLBuffer indexBuf;
+};
+
+namespace geom {
+
+    class QtGpuMesh : public GeometryEngine
+    {
+    public:
+        QtGpuMesh();
+        virtual ~QtGpuMesh(){}
+        // GeometryEngine:: : virtual void drawGeometry(QOpenGLShaderProgram *program);
+        // GeometryEngine:: : virtual void initGeometry();
+        // GeometryEngine:: : virtual int get_vertex_size();
+    };
+
+    class QtGpuSkin : public GeometryEngine
+    {
+    public:
+        QtGpuSkin();
+        virtual ~QtGpuSkin(){}
+        virtual void drawGeometry(QOpenGLShaderProgram *program);
+        // GeometryEngine:: : virtual void  initGeometry();
+        // GeometryEngine:: : virtual int  GeometryEngine :: get_vertex_size();
+    };
+
+    class node
+    {
+    public:
+        node(){}
+    };
+}
+
+#endif  //GX_GENERATIONGAP_BASE_H
+"""
+gx_cpp_face_template = """
+#include <gx_gap_interface.h>
+
+GeometryEngine::GeometryEngine() : indexBuf(QOpenGLBuffer::IndexBuffer) {
+    initializeOpenGLFunctions();
+    arrayBuf.create();
+    indexBuf.create();
+    initGeometry();
+}
+
+GeometryEngine::~GeometryEngine() {
+    arrayBuf.destroy();
+    indexBuf.destroy();
+}
+
+int GeometryEngine::get_vertices_count() { return 8164; }
+
+int GeometryEngine::get_indices_count() { return 47733; }
+
+int GeometryEngine::get_vertex_size() { return sizeof(MeshVertexData); }
+
+void GeometryEngine::initGeometry() {
+    MeshVertexData vertices[] = {
+        #include <builtins/Nikita0.gen.txt>
+    }; // len = 8164
+
+    GLushort indices[]    = {
+        #include <builtins/NikitaI.gen.txt>
+    }; // len = 47733
+
+    // Transfer vertex data to VBO 0
+    arrayBuf.bind();
+    arrayBuf.allocate(vertices, get_vertices_count() * get_vertex_size());
+
+    // Transfer index data to VBO 1
+    indexBuf.bind();
+    indexBuf.allocate(indices, get_indices_count() * sizeof(GLushort));
+}
+
+void GeometryEngine::drawGeometry(QOpenGLShaderProgram *program)
+{
+    // Tell OpenGL which VBOs to use
+    arrayBuf.bind();
+    indexBuf.bind();
+
+    // Offset for position
+    quintptr offset = 0;
+
+    /// POSITION
+    // Tell OpenGL programmable pipeline how to locate vertex position data
+    int vertexLocation = program->attributeLocation("a_position");
+    program->enableAttributeArray(vertexLocation);
+    program->setAttributeBuffer(vertexLocation, GL_FLOAT, offset, 3, sizeof(MeshVertexData));
+
+    // Offset for texture coordinate
+    offset += sizeof(QVector3D);
+
+    /// NORMAL
+    // Tell OpenGL programmable pipeline how to locate vertex position data
+    int normalLocation = program->attributeLocation("a_normal");
+    program->enableAttributeArray(normalLocation);
+    program->setAttributeBuffer(normalLocation, GL_FLOAT, offset, 3, sizeof(MeshVertexData));
+
+    offset += sizeof(QVector3D);
+
+    /// TEXCOORD_0
+    // Tell OpenGL programmable pipeline how to locate vertex texture coordinate data
+    int texcoordLocation = program->attributeLocation("a_texcoord");
+    program->enableAttributeArray(texcoordLocation);
+    program->setAttributeBuffer(texcoordLocation, GL_FLOAT, offset, 2, sizeof(MeshVertexData));
+
+    // Draw cube geometry using indices from VBO 1
+    // glDrawElements(GL_TRIANGLE_STRIP, 34, GL_UNSIGNED_SHORT, 0);
+    glDrawElements(GL_TRIANGLES, 47733, GL_UNSIGNED_SHORT, 0);
+}
+
+struct draw_commands : protected QOpenGLFunctions {};
+struct draw_variable_buffer : public draw_commands {
+    attr(const QString& name, GLenum type, int offset, int tupleSize, int stride = 0 );
+};
+struct draw_elements : draw_commands {
+    void paint() {
+        glDrawElements(GL_TRIANGLES, 47733, GL_UNSIGNED_SHORT, 0);
+    }
+};
+
+void geom::QtGpuSkin::drawGeometry(QOpenGLShaderProgram *program)
+{
+    // Tell OpenGL which VBOs to use
+    arrayBuf.bind();
+    indexBuf.bind();
+
+    // Offset for position
+    quintptr offset = 0;
+
+    /// ПЕРЕМЕЖЕННЫЙ МАССИВ АТТРИБУТОВ ВЕРШИН МАССИВ
+    /// int attribute_location, QString attribute_name локация
+    /// POSITION локация атрибута, тип атрибута
+    // Tell OpenGL programmable pipeline how to locate vertex position data
+    int vertexLocation = program->attributeLocation("a_position");
+    program->enableAttributeArray(vertexLocation);
+    program->setAttributeBuffer(vertexLocation, GL_FLOAT, offset, 3, sizeof(SkinVertexData));
+
+    // Offset for normal vector
+    offset += sizeof(QVector3D);
+
+    /// NORMAL
+    // Tell OpenGL programmable pipeline how to locate normal vector data
+    int normalLocation = program->attributeLocation("a_normal");
+    program->enableAttributeArray(normalLocation);
+    program->setAttributeBuffer(normalLocation, GL_FLOAT, offset, 3, sizeof(SkinVertexData));
+
+    offset += sizeof(QVector3D);
+
+    /// WEIGHTS_0
+    // Tell OpenGL programmable pipeline how to locate joint's WEIGHTS_0 vec4 data
+    int weights_0_location = program->attributeLocation("a_weights_0");
+    program->enableAttributeArray(weights_0_location);
+    program->setAttributeBuffer(weights_0_location, GL_FLOAT, offset, 4, sizeof(SkinVertexData));
+
+    offset += sizeof(QVector4D);
+
+    /// JOINTS_0
+    // Tell OpenGL programmable pipeline how to locate joint's WEIGHTS_0 vec4 data
+    int joints_0_location = program->attributeLocation("a_joints_0");
+    program->enableAttributeArray(joints_0_location);
+    program->setAttributeBuffer(joints_0_location, GL_FLOAT, offset, 4, sizeof(SkinVertexData));
+
+    offset += sizeof(QVector4D);
+
+    /// TEXCOORD_0
+    // Tell OpenGL programmable pipeline how to locate vertex texture coordinate data
+    int texcoordLocation = program->attributeLocation("a_texcoord");
+    program->enableAttributeArray(texcoordLocation);
+    program->setAttributeBuffer(texcoordLocation, GL_FLOAT, offset, 2, sizeof(SkinVertexData));
+
+    // Draw cube geometry using indices from VBO 1
+    // glDrawElements(GL_TRIANGLE_STRIP, 34, GL_UNSIGNED_SHORT, 0);
+    glDrawElements(GL_TRIANGLES, 47733, GL_UNSIGNED_SHORT, 0);
+}
+
+"""
+
+gx_pri_face_template = """# autogenerated GGAP head
+!contains ( INCLUDEPATH, $$PWD ) {
+  HEADERS     += $$PWD/gx_gap_interface.h
+  SOURCES     += $$PWD/gx_gap_interface.cpp
+  INCLUDEPATH += $$PWD
+}
+"""
